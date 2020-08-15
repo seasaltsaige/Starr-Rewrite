fc192e9cc726204461a07bd1b0643fedb888c050
Maxisthemoose <oriontothemax@gmail.com>
2020-08-15 00:27:28 -0700

    Comments in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
+// Import dotenv for environment variables
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config(); // Execute the config to bind env to the process
+
+// Import my Extended client
 import Client from "./utils/BaseClasses/StarrClient";
-require("./database/database");
+const StarrClient = new Client({ defaultPrefix: "?", commands: new Map(), owners: [ "408080307603111936" ] }); // Initialize the new client
+StarrClient.login(StarrClient.getToken()); // Login
+require("./database/database"); // Startup the database connection
 
-const StarrClient = new Client({
-    defaultPrefix: "?",
-    commands: new Map(), 
-    owners: [
-        "408080307603111936",
-    ],
-});
+import CommandHandler from "./handlers/CommandHandler"; // Import the command handler
+import EventHandler from "./handlers/EventHandler"; // Import the event handler
 
-StarrClient.login(StarrClient.getToken());
-import CommandHandler from "./handlers/CommandHandler";
-import EventHandler from "./handlers/EventHandler";
-CommandHandler.load("./src/commands", StarrClient);
+CommandHandler.load("./src/commands", StarrClient); // Execute both to initialize the commands and events
 EventHandler.load("./src/events", StarrClient);
