f6e96f365f0ef51c4d5a65ccf1aadc07ac70c2ba
Maxisthemoose <oriontothemax@gmail.com>
2020-09-05 12:30:09 -0700

    client.start() function (cleaned up index.ts)

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,11 @@
-// Import dotenv for environment variables
-import dotenv from "dotenv";
-dotenv.config(); // Execute the config to bind env to the process
-
 // Import my Extended client
 import Client from "./utils/BaseClasses/StarrClient";
+// Initialize the bot client
 const StarrClient = new Client({ 
     defaultPrefix: "?", 
     owners: [ "408080307603111936" ], 
     baseOptions: { 
-        partials: ["MESSAGE", "REACTION"],
+        partials: ["MESSAGE", "REACTION"] 
     } 
-}); // Initialize the new client
-
-import { Constants } from "discord.js";
-Constants.DefaultOptions.ws.properties.$browser = "Discord iOS";
-
-StarrClient.login(StarrClient.getToken()); // Login
-import("./database/database"); // Startup the database connection
-
-import CommandHandler from "./handlers/CommandHandler"; // Import the command handler
-import EventHandler from "./handlers/EventHandler"; // Import the event handler
-import { Categories } from "./utils/resolvables/Resolvables"; // Import the categories for the command handler
-
-CommandHandler.load("./src/commands", Categories, StarrClient); // Execute both to initialize the commands and events
-EventHandler.load("./src/events", StarrClient);
+});
+StarrClient.start(); // Start the client
--- src/utils/BaseClasses/StarrClient.ts
+++ src/utils/BaseClasses/StarrClient.ts
@@ -1,7 +1,11 @@
-import { Client, Snowflake, Guild, Channel, ClientOptions } from "discord.js"
+import { Client, Snowflake, Guild, Channel, ClientOptions, Constants } from "discord.js"
 import StarrClientInfo from "../types/StarrClientInfo";
 import GuildDoc from "../../database/models/Guild";
 import SnipeData from "../types/SnipeData";
+import CommandHandler from "../../handlers/CommandHandler"; // Import the command handler
+import EventHandler from "../../handlers/EventHandler"; // Import the event handler
+import { Categories } from "../../utils/resolvables/Resolvables"; // Import the categories for the command handler
+import { config } from "dotenv"; // Import config for environment variables
 
 export default class StarrClient extends Client {
     defaultPrefix: string;
@@ -28,18 +32,28 @@ export default class StarrClient extends Client {
             noColor: "#2F3136",
         }
     };
-    getToken(): string | undefined {
+
+    private getToken(): string | undefined {
         return process.env.BOT_TOKEN;
     }
-    async getGuildPrefix(guild: Guild) {
+
+    async getGuildPrefix(guild: Guild): Promise<string> {
         const foundGuild = await GuildDoc.findOne({ id: guild.id });
         if (!foundGuild) return null;
         const guildPrefix = foundGuild.prefix;
         return guildPrefix;
     }
-    getSnipe(client: StarrClient, guild: Guild, channel: Channel) {
+    getSnipe(client: StarrClient, guild: Guild, channel: Channel): SnipeData {
         const toget = JSON.stringify({ guild: guild.id, channel: channel.id });
         const snipedata = client.snipes.get(toget);
         return snipedata;
+    }
+    start(): void {
+        config(); // Execute the config to bind env to the process
+        Constants.DefaultOptions.ws.properties.$browser = "Discord iOS";
+        this.login(this.getToken());
+        import("../../database/database"); // Startup the database connection
+        CommandHandler.load("./src/commands", Categories, this); // Execute both to initialize the commands and events
+        EventHandler.load("./src/events", this);
     }
 }
